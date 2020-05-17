848a386c25f182458cb05b59a9c457fca41e3407
YassSoliman <soliman.yasser@hotmail.com>
2020-05-17 16:11:46 -0400

    Modification

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -4,14 +4,16 @@
         <meta charset="UTF-8">
         <base href="<?= $racineWeb ?>" >
         <link rel="stylesheet" href="Contenue/css/style.css" />
-        <title><?= $titre ?></title>        
+        <title><?= $titre ?></title>
     </head>
     <body>
         <div id="global">
             <header>
                 <a href="index.php"><h1 id="titreInventaire">L'inventaire de Yasser</h1></a>
                 <p>Version avec Cadriciel (Framework) MVC maison : Ajout d'effacement (au lieu de suppression) et de rétablissement des produits.</p>
-                <a href="tests.php"><h1 id="titreBlog">TESTS</h1></a>
+                <a href="<?= $utilisateur != '' ? 'Admin' : ''; ?>Commande">
+                    <h4>Afficher tous les produits de tous les commandes</h4>
+                </a>
                 <a href="Apropos">À propos</a>
             </header>
             <?php if ($utilisateur != '') : ?>
